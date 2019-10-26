5def1259921875cb361d9b3ab432d7a442fc87e3
jugujo <jugujoo@hotmail.com>
2019-10-26 14:46:32 +0900

    up

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-30.times do |i|
-    Board.create(user_id:1, content: "hi #{i}")
+5.times do |i|
+    # Board.create(user_id:1, content: "hi #{i}")
+    #Category.create(name:"hi #{i}", content: "hi #{i}")
 end
+
+Category.create(name:"Sqare", description: "This is a sqare")
+Category.create(name:"Circle", description: "This is a circle")
+Category.create(name:"Triangle", description: "This is a triangle")
